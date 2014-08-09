48b510650378576de56091781d05a0a3992a2a59
Kumar Anirudha <anirudhastark@yahoo.com>
2014-08-09 14:39:30 +0530

    Updated

---
+++ js/extra.js
@@ -0,0 +1,18 @@
+var map = L.map('map').setView([22.56, 88.36], 14);
+
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	id: 'examples.map-i86knfo3'
+}).addTo(map);
+
+function onMapClick(e) {
+	popup
+		.setLatLng(e.latlng)
+		.setContent("You clicked the map at " + e.latlng.toString())
+		.openOn(map);
+}
+
+map.on('click', onMapClick);
